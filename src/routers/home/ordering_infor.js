4a626cdba5295f0594e340b204b2efc3eefe47de
K QUOC <your.email@example.com>
2024-05-08 23:20:57 +0700

    change

--- src/routers/home/ordering_infor.js
+++ src/routers/home/ordering_infor.js
@@ -16,12 +16,34 @@ const OrderingInfoPage = () => {
     try {
       const functionName = "getOrderingInfo";
       const result = await app.currentUser.callFunction(functionName);
-      setProducts(result); // Cập nhật state products với dữ liệu từ server
+      setProducts(result); // Cập nhật state products với dữ liệu từ server;
       setLoading(false);
     } catch (error) {
       console.log(error.error);
     }
   };
+
+  const handleShowProducts = async (productId) => {
+    try {
+      // Gọi API để lấy thông tin sản phẩm cụ thể dựa trên productId
+      const functionName = "getOrderingInfo";
+      const productDetail = await app.currentUser.callFunction(functionName,productId);
+      
+      // Cập nhật trạng thái của sản phẩm
+      setProducts(products.map(product => {
+        if (product._id === productId) {
+          return { ...product, selected: !product.selected }; // Đảo ngược trạng thái selected
+        } else {
+          return product;
+        }
+      }));
+
+      return productDetail;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   return (
     <div className="container_OderingPage">
@@ -34,7 +56,7 @@ const OrderingInfoPage = () => {
               {Array.isArray(products) && products.length > 0 ? (
                 products.map((product, index) => (
                   <div key={index} className="products">
-                    <h3>Thông tin sản phẩm {index + 1}:</h3>
+                    <h3>Thông tin đơn hàng {index + 1}</h3>
                     <img src={product.imageUrl} alt={product.imageUrl} />
                     <p><strong>Tên khách hàng:</strong> {product.fullName}</p>
                     <p><strong>Id khách hàng:</strong> {product.user}</p>
@@ -42,6 +64,21 @@ const OrderingInfoPage = () => {
                     <p><strong>Email:</strong> {product.email}</p>
                     <p><strong>Address:</strong> {product.address}</p>
                     <p><strong>Số điện thoại:</strong> {product.phoneNumber}</p>
+                    <p><strong>Tổng thanh toán:</strong> ${product.totalPays}</p>
+                    <button onClick={() => handleShowProducts(product._id)}>
+                      {product.selected ? "Ẩn" : "Chi tiết"} {/* Thay đổi văn bản của nút */}
+                    </button>
+                    {product.selected && product.products.map((productlist, productIndex) => (
+                      <div key={productIndex}>
+                        <p><strong>Sản phẩm {productIndex + 1}</strong></p>
+                        <p><strong>Tên sản phẩm:</strong> {productlist.productName}</p>
+                        <p><strong>Loại sản phẩm:</strong> {productlist.productType}</p>
+                        <p><strong>Giá bán:</strong> ${productlist.sellingPrice}</p>
+                        <p><strong>Số lượng mua:</strong> {productlist.productCounts}</p>
+                        <p><strong>Tổng thanh toán:</strong> ${productlist.totalPay}</p>
+                        {/* Các thông tin khác về sản phẩm */}
+                      </div>
+                    ))}
                   </div>
                 ))
               ) : (
