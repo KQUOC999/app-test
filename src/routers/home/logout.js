8dfb7a54b2872ff123342af49b2d1f4f271fa1f2
K QUOC <your.email@example.com>
2024-05-21 16:45:19 +0700

    change

--- src/routers/home/logout.js
+++ src/routers/home/logout.js
@@ -7,6 +7,7 @@ import Home from './page';
 
 
 const app = new Realm.App({ id: process.env.REACT_APP_REALM_ID });
+const user = app.currentUser;
 
 const schema = {
   title: 'Register',
@@ -29,7 +30,7 @@ const loginSchema = {
 };
 
 const Logout = () => {
-  const [user, setUser] = useState(null);
+  const [ , setUser] = useState(null);
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const [loading, setLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Thêm state mới để kiểm tra trạng thái đăng nhập
@@ -42,8 +43,7 @@ const Logout = () => {
           // Authenticate the user
           if (user) { // Kiểm tra xem user có tồn tại không trước khi gọi fetchUser
             await user.logOut(); // Trước khi đăng xuất, kiểm tra user có tồn tại
-            register();
-            login();
+            window.location.reload(true)
           }
         } catch (error) {
           console.log(error.error);
@@ -54,7 +54,7 @@ const Logout = () => {
         fetchUser();
       }
       
-  }, [user]); // Thêm logOut vào danh sách dependency của useEffect
+  }, []); // Thêm logOut vào danh sách dependency của useEffect
 
   const register = async (form) => {
     const { email, password } = form.formData;
