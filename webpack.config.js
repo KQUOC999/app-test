c4c5d5a71863c5b60cfb4edce488cef4789c9b8f
K QUOC <your.email@example.com>
2024-05-22 14:58:41 +0700

    change

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production', // Đảm bảo rằng Webpack đang ở chế độ production
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new JavaScriptObfuscator({
+            rotateUnicodeArray: true
+        }, ['excluded_bundle_name.js']) // Đặt tên tệp bạn không muốn obfuscate
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    }
+};
